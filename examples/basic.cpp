fac477bd2d02da14f85a6b2b9752a6a3e11e0e85
n-arms <75282394+n-arms@users.noreply.github.com>
2024-11-09 10:38:28 -0500

    Implement template magic for property-based tests

--- examples/basic.cpp
+++ examples/basic.cpp
@@ -1,5 +1,23 @@
 #include "../include/magic.hpp"
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <cassert>
 
 int main() {
-  say_hello();
+  std::srand(std::time(nullptr));
+
+  magictest<int, int>(
+    [](int f_x, int x) {
+      assert(f_x == x);
+    },
+    [](int x) {
+      return x;
+    },
+    {
+      []() {
+        return std::rand();
+      }
+    }
+  );
 }
--- include/magic.hpp
+++ include/magic.hpp
@@ -1,7 +1,32 @@
 #pragma once
 
-#include <iostream>
+#include <tuple>
+#include <functional>
 
-void say_hello() {
-  std::cout << "Hello World" << std::endl;
+template <typename... Args>
+using args = std::tuple<std::function<Args()>...>;
+
+template <typename... Args, std::size_t... I>
+std::tuple<Args...> generate_args_for(args<Args...> arguments, std::index_sequence<I...> indices) {
+  return std::tuple<Args...> { std::get<I>(arguments)()... };
+}
+
+template <typename... Args>
+std::tuple<Args...> generate_args(args<Args...> arguments) {
+  return generate_args_for<Args...>(arguments, std::index_sequence_for<Args...>());
+}
+
+template <typename Res, typename... Args>
+void magictest(
+  std::type_identity_t<std::function<void(Res, Args...)>> check,
+  std::type_identity_t<std::function<Res(Args...)>> function,
+  args<Args...> arguments
+) {
+  std::tuple<Args...> args = generate_args(arguments);
+  Res result = std::apply(function, args);
+  std::tuple<Res, Args...> check_args = std::tuple_cat(
+    std::tuple<Res> { result },
+    args
+  );
+  std::apply(check, check_args);
 }
